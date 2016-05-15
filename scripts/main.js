b39d861fa6fd6b3b506f4bdba45bae61c610f07a
iain plimmer <iainplimmer@iains-iMac.local>
2016-05-15 20:45:54 +0100

    Bit of self documentation after getting this working......

--- scripts/main.js
+++ scripts/main.js
@@ -3,4 +3,4 @@ var greetings = require("./greetings.js");
 
 q("div").css('background','#ff0000');
 q("div").css('color','#ffffff');
-q("div").text(greetings.sayHelloInSpanish());
+q("div").text(greetings.sayHelloInSpanish() + ' i think you will find this works!');
