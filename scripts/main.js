b310a5c740d7f7eea9114ba9e9fde5984c4af971
iain plimmer <iainplimmer@iains-iMac.local>
2016-05-15 20:41:52 +0100

    First commit

---
+++ scripts/main.js
@@ -0,0 +1,6 @@
+var q = require('jq');
+var greetings = require("./greetings.js");
+
+q("div").css('background','#ff0000');
+q("div").css('color','#ffffff');
+q("div").text(greetings.sayHelloInSpanish());
